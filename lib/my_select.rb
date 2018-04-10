03f5861ac098887ae66bf587eeee2b95982b3e0d
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-10 22:11:10 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ if block_given?
+   i = 0
+   while i < collection.length
+     collection.select do |ele|
+       yield(collection[i])
+       i += 1
+    end   
+ end
 end
